e11a5b13c5e8a83fa21fec9208e19bdeba5e3037
Asad Mahmood <asad.mahmood@arabianpay.net>
2025-07-27 09:45:42 +0300

    Product Media Helper

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -129,7 +129,7 @@
                                                 <td class="text-center">{{ $product->id }}</td>
                                                 <td>
                                                     @if ($product->thumbnail)
-                                                        <img src="{{ productMedia($product->thumbnail) }}"
+                                                        <img src="{{ supplierMedia($product->thumbnail) }}"
                                                             alt="{{ $product->name }}"
                                                             class="w-10 h-10 object-contain border-7">
                                                     @else
