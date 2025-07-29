a0440728b216efb6a4683253b9aebcc7fa21d002
Asad Mahmood <asad.mahmood@arabianpay.net>
2025-07-29 10:23:29 +0300

    Products Filter

--- resources/views/layouts/includes/table-pagination.blade.php
+++ resources/views/layouts/includes/table-pagination.blade.php
@@ -1,8 +1,19 @@
+@php
+    function appendQueryParams($url)
+    {
+        $query = request()->except('page');
+        if (count($query)) {
+            $url .= (str_contains($url, '?') ? '&' : '?') . http_build_query($query);
+        }
+        return $url;
+    }
+@endphp
+
 @if ($paginator->hasPages())
     <div
         class="card-footer justify-center md:justify-between flex-col md:flex-row gap-5 text-gray-600 text-2sm font-medium">
 
-        <!-- Pagination Links -->
+        <!-- Pagination Info -->
         <div class="flex items-center gap-4 order-1 md:order-2">
             <span>
                 {{ translate('Showing') }} {{ $paginator->firstItem() }} {{ translate('to') }}
@@ -14,7 +25,7 @@
                 @if ($paginator->onFirstPage())
                     <span class="px-3 py-1 text-gray-400">{{ translate('Previous') }}</span>
                 @else
-                    <a href="{{ $paginator->previousPageUrl() }}{{ request()->has('perpage') ? '&perpage=' . request('perpage') : '' }}"
+                    <a href="{{ appendQueryParams($paginator->previousPageUrl()) }}{{ request()->has('perpage') ? '&perpage=' . request('perpage') : '' }}"
                         class="px-3 py-1 text-blue-600 hover:underline">{{ translate('Previous') }}</a>
                 @endif
 
@@ -27,7 +38,7 @@
                             <span
                                 class="px-3 py-1 bg-gray-300 text-gray-800 rounded shadow-sm">{{ $label }}</span>
                         @else
-                            <a href="{{ $element['url'] }}{{ request()->has('perpage') ? '&perpage=' . request('perpage') : '' }}"
+                            <a href="{{ appendQueryParams($element['url']) }}{{ request()->has('perpage') ? '&perpage=' . request('perpage') : '' }}"
                                 class="px-3 py-1 text-blue-600 hover:underline rounded">{{ $label }}</a>
                         @endif
                     @endif
@@ -35,7 +46,7 @@
 
                 {{-- Next Page Link --}}
                 @if ($paginator->hasMorePages())
-                    <a href="{{ $paginator->nextPageUrl() }}{{ request()->has('perpage') ? '&perpage=' . request('perpage') : '' }}"
+                    <a href="{{ appendQueryParams($paginator->nextPageUrl()) }}{{ request()->has('perpage') ? '&perpage=' . request('perpage') : '' }}"
                         class="px-3 py-1 text-blue-600 hover:underline">{{ translate('Next') }}</a>
                 @else
                     <span class="px-3 py-1 text-gray-400">{{ translate('Next') }}</span>
