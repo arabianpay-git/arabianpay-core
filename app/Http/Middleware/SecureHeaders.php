3d040eb8aa16c9935928106e0e53afe2194aa468
Asad Mahmood <asad.mahmood@arabianpay.net>
2025-10-30 16:51:14 +0300

    Updates

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -44,7 +44,7 @@ class SecureHeaders
             "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://code.jquery.com https://cdn.jsdelivr.net https://www.gstatic.com https://www.googleapis.com {$viteDevServer};",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net {$viteDevServer};",
             "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net {$viteDevServer};",
-            "img-src 'self' data: https://core.arabianpay.net https://partners.arabianpay.net https://ui-avatars.com https://cd.salla.sa {$viteDevServer};",
+            "img-src 'self' data: https://core.arabianpay.net https://partners.arabianpay.net https://ui-avatars.com https://cdn.salla.sa {$viteDevServer};",
             "connect-src 'self' https://fcm.googleapis.com https://firebase.googleapis.com https://www.googleapis.com https://www.gstatic.com https://firebaseinstallations.googleapis.com https://fcmregistrations.googleapis.com ws://localhost:5174 ws://localhost:5173 wss://merchant.test:5174 wss://merchant.test:5173;",
             "frame-src https://www.gstatic.com;",
             "frame-ancestors 'none';",
