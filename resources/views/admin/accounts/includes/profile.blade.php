8d2bdea505ca933bc0d5509a0441e5c1d6d26210
Asad Mahmood <asad.mahmood@arabianpay.net>
2025-12-02 16:37:24 +0300

    Simah Consumer Score

--- resources/views/admin/accounts/includes/profile.blade.php
+++ resources/views/admin/accounts/includes/profile.blade.php
@@ -3,9 +3,20 @@
         <div
             class="flex items-center justify-center rounded-full border-2 border-success-clarity size-[100px] shrink-0 bg-light">
             @php
+                // 1. Check shop logo
                 $shopLogo = \App\Models\ShopSetting::where('user_id', $merchant->user_id)->first();
+                $logoPath = null;
 
-                $logoPath = $shopLogo && $shopLogo->logo ? $shopLogo->logo : asset('assets/media/images/ap.png');
+                if ($shopLogo && $shopLogo->logo) {
+                    // Show Shop Logo
+                    $logoPath = $shopLogo->logo;
+                } elseif ($merchant->user && $merchant->user->profile_photo_path) {
+                    // 2. Show User Profile Photo (from core/public/profile-photos)
+                    $logoPath = asset('profile-photos/' . $merchant->user->profile_photo_path);
+                } else {
+                    // 3. Default Image
+                    $logoPath = asset('assets/media/images/ap.png');
+                }
             @endphp
             <img class="size-[70px]" src="{{ $logoPath }}" alt="Shop Logo" />
 
